41ecde84c0203f6630ee8a1f5be620e6590d8d91
bjsamuel <chinnabhai944@gmail.com>
2020-04-13 18:04:17 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'hddng': ['Poppins'],
+    },
+    colors: {
+      pink: '#ff52bf',
+      white: '#FFFFFF',
+      gray: "#718096",
+      paleblue: '#f5faff',
+      DarkCyan   :'#00252e',
+      black : "#000000"
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      spacing: {
+      '29': '29rem',
+      },
+      width: {
+        '37': '37%',
+        '54':'54rem',
+        '64p':"64%",
+        '84p': "84%",
+        '21r':'21rem'
+      },
+    },
+    screens: {
+      'mb': {'max': '375px'},
+    },
+  },
+
+
+  variants: {},
+  plugins: [],
+}
